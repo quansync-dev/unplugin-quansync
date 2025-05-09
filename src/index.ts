c66ad642d6de9fa9ed07c1ee69294f73bb9a2fa7
三咲智子 Kevin Deng <sxzz@sxzz.moe>
2025-05-09 08:06:02 +0800

    perf: prefer native filter

--- src/index.ts
+++ src/index.ts
@@ -1,25 +1,27 @@
 import { createUnplugin, type UnpluginInstance } from 'unplugin'
-import { createFilter } from 'unplugin-utils'
 import { transformQuansync } from './core'
 import { resolveOptions, type Options } from './core/options'
 
 export const Quansync: UnpluginInstance<Options | undefined, false> =
   createUnplugin((rawOptions = {}) => {
     const options = resolveOptions(rawOptions)
-    const filter = createFilter(options.include, options.exclude)
 
     const name = 'unplugin-quansync'
     return {
       name,
       enforce: options.enforce,
 
-      transformInclude(id) {
-        return filter(id)
-      },
-
-      transform(code, id) {
-        if (!code.includes('quansync')) return
-        return transformQuansync(code, id)
+      transform: {
+        filter: {
+          id: {
+            include: options.include,
+            exclude: options.exclude,
+          },
+        },
+        handler(code, id) {
+          if (!code.includes('quansync')) return
+          return transformQuansync(code, id)
+        },
       },
     }
   })
