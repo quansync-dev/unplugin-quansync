9608a58cf4165c4df1cc81586cbf22128c7f99e6
Kevin Deng <sxzz@sxzz.moe>
2026-03-14 00:07:16 +0800

    chore: upgrade deps

--- tsdown.config.ts
+++ tsdown.config.ts
@@ -1,7 +1,5 @@
-import { defineConfig } from 'tsdown'
+import { lib } from 'tsdown-preset-sxzz'
 
-export default defineConfig({
-  entry: './src/*.ts',
-  exports: true,
-  inlineOnly: [],
+export default lib({
+  entry: 'shallow',
 })
